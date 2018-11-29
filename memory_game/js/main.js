c44564a445ce897d5f70101a45cae4a1976a8298
Sithiz <Joshklaas1@gmail.com>
2018-11-28 18:47:04 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+
+console.log('User flipped ' + cardOne)
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardTwo)
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1] ){
+		alert('You Found a Match!');
+	}
+	else {
+		alert('Sorry! try again.');
+
+	}
+}

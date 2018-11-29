77d42449da145af11226f1d6ffbccd8676d6a59c
Sithiz <Joshklaas1@gmail.com>
2018-11-29 11:27:40 -0600

    created game logicv2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,26 @@
 
 var cards = ["queen","queen","king","king"];
-
 var cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-
-console.log('User flipped ' + cardOne)
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardTwo)
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1] ){
 		alert('You Found a Match!');
-	}
+		}
 	else {
 		alert('Sorry! try again.');
 
+		}
 	}
 }
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+ 	checkForMatch();
+
+}
+
+
+
+
+flipCard(0);
+flipCard(2);

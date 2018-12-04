b951fcd4326694f7f8688d16bec90acbd4a4827b
Sithiz <Joshklaas1@gmail.com>
2018-12-03 21:22:29 -0600

    created game logicv4

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,7 +25,7 @@ var cards = [
 ];
 var cardsInPlay = [];
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2) {
+	
 	if (cardsInPlay[0] === cardsInPlay[1] ){
 		alert('You Found a Match!');
 		}
@@ -33,19 +33,31 @@ var checkForMatch = function() {
 		alert('Sorry! try again.');
 
 		}
-	}
+	
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+ 	this.setAttribute('src', cards[cardId].cardImage);
+ 	if (cardsInPlay.length === 2){
  	checkForMatch();
+ }
  	console.log(cards[cardId].cardImage);
  	console.log(cards[cardId].suit);
 
+
 }
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png")
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement) 
+		
+	}
+	}
 
-
-
-flipCard(0);
-flipCard(2);
+createBoard();
